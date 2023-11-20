f2f4c6428998d88920e607bdd4ced3099e26b988
pushanwebart <pushan.chakraborty@webart.technology>
2023-11-20 19:43:15 +0530

    plz check home update

--- wp-content/themes/collisiontheme/page-templates/index.php
+++ wp-content/themes/collisiontheme/page-templates/index.php
@@ -10,14 +10,26 @@ $generalpageid = get_id_by_slug($page_slug);
     <section class="main-slider">
         <div class="rev_slider_wrapper fullwidthbanner-container"  id="rev_slider_one_wrapper" data-source="gallery">
             <div class="rev_slider fullwidthabanner" id="rev_slider_one" data-version="5.4.1">
-         <?php        $homeslides = CFS()->get( 'sliderepeat' ); ?>
+         <?php       // $homeslides = CFS()->get( 'sliderepeat' ); 
+
+         $homeslides = CFS()->get( 'toprevslides'); 
+
+              
+
+         ?>
   
                 <ul>
-                 <?php    foreach ( $homeslides as $eachslide ) {  ?>
-                    <!-- Slide 1 -->
-                    <li data-description="Slide Description" data-easein="default" data-easeout="default" data-fsmasterspeed="1500" data-fsslotamount="7" data-fstransition="fade" data-hideafterloop="0" data-hideslideonmobile="off" data-index="rs-1687" data-masterspeed="default" data-param1="" data-param10="" data-param2="" data-param3="" data-param4="" data-param5="" data-param6="" data-param7="" data-param8="" data-param9="" data-rotate="0" data-saveperformance="off" data-slotamount="default" data-thumb="images/main-slider/image-1.jpg" data-title="Slide Title" data-transition="parallaxvertical">
+                 <?php    foreach ( $homeslides as $eachslide ) { 
 
-                        <img alt="" class="rev-slidebg" data-bgfit="cover" data-bgparallax="10" data-bgposition="center center" data-bgrepeat="no-repeat" data-no-retina="" src="<?php echo $eachslide['home_slide_image']; ?>">
+                    echo $eachslide['list_items'];
+
+
+
+                  ?>
+                    <!-- Slide 1 -->
+                  <!--  <li data-description="Slide Description" data-easein="default" data-easeout="default" data-fsmasterspeed="1500" data-fsslotamount="7" data-fstransition="fade" data-hideafterloop="0" data-hideslideonmobile="off" data-index="rs-1687" data-masterspeed="default" data-param1="" data-param10="" data-param2="" data-param3="" data-param4="" data-param5="" data-param6="" data-param7="" data-param8="" data-param9="" data-rotate="0" data-saveperformance="off" data-slotamount="default" data-thumb="images/main-slider/image-1.jpg" data-title="Slide Title" data-transition="parallaxvertical">
+
+                        <img alt="" class="rev-slidebg" data-bgfit="cover" data-bgparallax="10" data-bgposition="center center" data-bgrepeat="no-repeat" data-no-retina="" src="<?php //echo $eachslide['home_slide_image']; ?>">
 
                         <div class="tp-caption"
                         data-paddingbottom="[0,0,0,0]"
@@ -34,7 +46,7 @@ $generalpageid = get_id_by_slug($page_slug);
                         data-x="['left','left','left','left']"
                         data-y="['middle','middle','middle','middle']"
                         data-frames='[{"from":"y:[100%];z:0;rX:0deg;rY:0;rZ:0;sX:1;sY:1;skX:0;skY:0;","mask":"x:0px;y:0px;s:inherit;e:inherit;","speed":1500,"to":"o:1;","delay":1000,"ease":"Power3.easeInOut"},{"delay":"wait","speed":1000,"to":"auto:auto;","mask":"x:0;y:0;s:inherit;e:inherit;","ease":"Power3.easeInOut"}]'>
-                            <h4><?php echo $eachslide['top_text']; ?></h4>
+                            <h4><?php //echo $eachslide['top_text']; ?></h4>
                         </div>
 
                         <div class="tp-caption"
@@ -52,7 +64,7 @@ $generalpageid = get_id_by_slug($page_slug);
                         data-x="['left','left','left','left']"
                         data-y="['middle','middle','middle','middle']"
                         data-frames='[{"from":"y:[100%];z:0;rX:0deg;rY:0;rZ:0;sX:1;sY:1;skX:0;skY:0;","mask":"x:0px;y:0px;s:inherit;e:inherit;","speed":1500,"to":"o:1;","delay":1000,"ease":"Power3.easeInOut"},{"delay":"wait","speed":1000,"to":"auto:auto;","mask":"x:0;y:0;s:inherit;e:inherit;","ease":"Power3.easeInOut"}]'>
-                            <h2><?php echo $eachslide['big_text']; ?></h2>
+                            <h2><?php //echo $eachslide['big_text']; ?></h2>
                         </div>
 
                         <div class="tp-caption"
@@ -70,7 +82,7 @@ $generalpageid = get_id_by_slug($page_slug);
                         data-x="['left','left','left','left']"
                         data-y="['middle','middle','middle','middle']"
                         data-frames='[{"from":"y:[-100%];z:0;rX:0deg;rY:0;rZ:0;sX:1;sY:1;skX:0;skY:0;","mask":"x:0px;y:0px;s:inherit;e:inherit;","speed":1500,"to":"o:1;","delay":1000,"ease":"Power3.easeInOut"},{"delay":"wait","speed":1000,"to":"auto:auto;","mask":"x:0;y:0;s:inherit;e:inherit;","ease":"Power3.easeInOut"}]'>
-                            <div class="text"><?php echo $eachslide['short_desc']; ?></div>
+                            <div class="text"><?php //echo $eachslide['short_desc']; ?></div>
                         </div>
 
                         <div class="tp-caption tp-resizeme"
@@ -88,11 +100,16 @@ $generalpageid = get_id_by_slug($page_slug);
                         data-x="['left','left','left','left']"
                         data-y="['middle','middle','middle','middle']"
                         data-frames='[{"from":"y:[100%];z:0;rX:0deg;rY:0;rZ:0;sX:1;sY:1;skX:0;skY:0;","mask":"x:0px;y:0px;s:inherit;e:inherit;","speed":1500,"to":"o:1;","delay":1000,"ease":"Power3.easeInOut"},{"delay":"wait","speed":1000,"to":"auto:auto;","mask":"x:0;y:0;s:inherit;e:inherit;","ease":"Power3.easeInOut"}]'>
-                            <a href="<?php echo $eachslide['book_now_link']; ?>" class="theme-btn btn-style-one"><?php echo $eachslide['book_now_text']; ?></a>
+                            <a href="<?php // echo $eachslide['book_now_link']; ?>" class="theme-btn btn-style-one"><?php //echo $eachslide['book_now_text']; ?></a>
                         </div>
-                    </li>
+                    </li> -->
 
-                <?php } ?>
+                <?php 
+
+
+            } 
+
+            ?>
 
                    
 
